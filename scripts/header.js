7e240b3f1861b1d99f7d30c709c430d6c884dff9
sbrumley <sbrumley@wireless-town.com>
2016-05-22 17:11:01 -0400

    breaking up page into javascript pieces.  Added Table and Main back for testing.

---
+++ scripts/header.js
@@ -0,0 +1,28 @@
+/**
+ * Created by sbrumley on 5/22/16.
+ */
+document.write(' \
+<header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary"> \
+  <div class="mdl-layout--large-screen-only mdl-layout__header-row"> \
+  </div> \
+  <div class="mdl-layout__header-row"> \
+');
+  document.write('<h3>' + nameStr + '</h3> ');
+document.write(' \
+<h3>&nbsp;Wi-Fi</h3> \
+</div> \
+<div class="mdl-layout--large-screen-only mdl-layout__header-row"> \
+  </div> \
+  <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark"> \
+  <a href="#overview" class="mdl-layout__tab is-active">Overview</a> \
+  <a href="terms.html" class="mdl-layout__tab">Terms of use</a> \
+<a href="#features" class="mdl-layout__tab">Details</a> \
+  <a href="#features" class="mdl-layout__tab">Technology</a> \
+  <a href="#features" class="mdl-layout__tab">FAQ</a> \
+  <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent" id="add"> \
+  <i class="material-icons" role="presentation">add</i> \
+  <span class="visuallyhidden">Add</span> \
+  </button> \
+  </div>  \
+</header> \
+');
